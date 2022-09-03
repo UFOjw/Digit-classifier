a8ebe8ff09f404ff5aee36cb2450fe632d70d53a
UFOjw <95556055+UFOjw@users.noreply.github.com>
2022-09-04 00:35:34 +0300

    Update train_and_eval.py

--- train_and_eval.py
+++ train_and_eval.py
@@ -10,7 +10,10 @@ x_test = np.reshape(x_test, (10000, 784)).T / 255
 
 W1, b1, W2, b2 = gradient_descent(x_train, y_train, 0.10, 500)
 
-test_prediction(0, W1, b1, W2, b2, x_train, y_train)
-test_prediction(1, W1, b1, W2, b2, x_train, y_train)
-test_prediction(2, W1, b1, W2, b2, x_train, y_train)
-test_prediction(3, W1, b1, W2, b2, x_train, y_train)
+test_prediction(0, W1, b1, W2, b2, x_test, y_test)
+test_prediction(1, W1, b1, W2, b2, x_test, y_test)
+test_prediction(2, W1, b1, W2, b2, x_test, y_test)
+test_prediction(3, W1, b1, W2, b2, x_test, y_test)
+
+predictions = make_predictions(x_test, W1, b1, W2, b2)
+print(get_accuracy(predictions, y_test))
