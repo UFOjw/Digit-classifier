3b48f463077504547cbba8d3202cd7950906d673
UFOjw <95556055+UFOjw@users.noreply.github.com>
2022-09-04 00:22:11 +0300

    Update train_and_eval.py

--- train_and_eval.py
+++ train_and_eval.py
@@ -6,7 +6,7 @@ from keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = np.reshape(x_train, (60000, 784)).T / 255
-x_test = np.reshape(x_train, (60000, 784)).T / 255
+x_test = np.reshape(x_test, (10000, 784)).T / 255
 
 W1, b1, W2, b2 = gradient_descent(x_train, y_train, 0.10, 500)
 
